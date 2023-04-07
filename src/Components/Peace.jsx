48066ed38f3f4f1ad9dbee60c5fe4e848d5c3418
MD OWAIS ATHAR <owais.athar.8092@gmail.com>
2023-04-07 15:45:08 +0530

    responsive for all screens and cart completed

--- src/Components/Peace.jsx
+++ src/Components/Peace.jsx
@@ -2,13 +2,14 @@ import { Box, Button, Flex, Heading, Text } from '@chakra-ui/react'
 import React from 'react'
 
 const Peace = () => {
+
     return (
         <Flex flexWrap="wrap" justifyContent="center" p={10}>
             <Box width={{ base: "100%", sm: "45%", md: "55%" }} margin={2} textColor={"white"} background={"black"} pt={40} pb={40}>
                 <Heading mb={5} fontWeight={"semibold"} fontSize={"3xl"}>PEACE OF MIND</Heading>
                 <Text>A one stop platform for all your fashion needs,</Text>
                 <Text mb={"25px"}>hassle-free. Buy with a peace of mind.</Text>
-                <Button color={"#024e82"}>Buy Now</Button>
+                <Button color={"#024e82"} >Buy Now</Button>
             </Box>
             <Box width={{ base: "100%", sm: "45%", md: "35%" }} margin={2} textColor={"white"} background={"black"} pt={40} pb={40}>
                 <Heading mb={5} fontWeight={"semibold"} fontSize={"3xl"}>BUY 2 GET 1 FREE</Heading>
--- src/Pages/HomePage.jsx
+++ src/Pages/HomePage.jsx
@@ -6,14 +6,26 @@ import front from "../assets/front.PNG"
 import { Image } from '@chakra-ui/react'
 import Navbar from "../Components/Navbar"
 import Footer from "../Components/Footer"
+import { useRef } from "react"
 
 const HomePage = () => {
+  const newarrivalRef = useRef(null);
+
+  const handleBuyNowClick = () => {
+    if (newarrivalRef.current) {
+      window.scrollTo({
+        top: newarrivalRef.current.offsetTop,
+        behavior: 'smooth',
+      });
+    }
+  }
+
 
   return (
    <>
    <Navbar />
-    <Image src={front} />
-    <section>
+    <Image src={front} onClick={handleBuyNowClick} cursor={"pointer"}/>
+    <section ref={newarrivalRef}>
         <NewArrivals />
     </section>
     
